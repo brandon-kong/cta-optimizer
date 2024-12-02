a9f613ed8fe2fb432ef9a484d6bd97ca7f76fc85
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-12-02 15:58:00 -0600

    feat: added utility functions for calculating distance

---
+++ src/utils/conversion.py
@@ -0,0 +1,11 @@
+def kilometers_to_miles(kilometers: float) -> float:
+    """
+    Converts kilometers to miles.
+    """
+    return kilometers * 0.621371
+
+def miles_to_kilometers(miles: float) -> float:
+    """
+    Converts miles to kilometers.
+    """
+    return miles * 1.60934
