84e5dabb1d1a9d6c55dfc95f765c14cc3b5780ec
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-12-07 19:53:43 -0600

    feat: added django backend

---
+++ backend/services/graph_service.py
@@ -0,0 +1,7 @@
+"""
+graph_service.py
+----------------
+The GraphService class is responsible for managing the graph building of the train system
+and optimization logic for finding the best path between the source and destination stations.
+"""
+
